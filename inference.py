a060e75fc43ec48287b6775f6a775c9322e5db56
DarokCx <77368869+DarokCx@users.noreply.github.com>
2024-02-23 10:23:55 -0800

    batches

--- inference.py
+++ inference.py
@@ -86,7 +86,7 @@ for i in range(len(testdata[0])):
 data = extract_column(dataset_name, split, column_name)
 
 batchsize = 70
-batches = 10
+batches = 100
 with open("output.txt", "w") as f:
     for step in range(0, batches*batchsize, batchsize):
         
