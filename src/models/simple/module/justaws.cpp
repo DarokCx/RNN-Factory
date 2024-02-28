7c0774b5797811116393f5f49af4477439a1419b
Harrison <harrison@recursal.ai>
2024-02-27 16:05:27 -0800

    justaws

--- src/models/simple/module/justaws.cpp
+++ src/models/simple/module/justaws.cpp
@@ -1,15 +1,13 @@
 #include <torch/extension.h>
 #include "ATen/ATen.h"
 
-#include "torchneuron/register.h"
-
 typedef at::BFloat16 bf16;
 typedef at::Half fp16;
 typedef float fp32;
 
 
 
-torch::Tensor forward_cpu(int64_t B, int64_t T, int64_t C, int64_t H, torch::Tensor &s, torch::Tensor &r, torch::Tensor &k, torch::Tensor &v, torch::Tensor &w, torch::Tensor &u) {
+torch::Tensor forward_cpu_compute(int64_t B, int64_t T, int64_t C, int64_t H, torch::Tensor &s, torch::Tensor &r, torch::Tensor &k, torch::Tensor &v, torch::Tensor &w, torch::Tensor &u) {
     
     auto rr = r.accessor<float, 4>();
     auto kk = k.accessor<float, 4>();
@@ -75,7 +73,3 @@ torch::Tensor forward_cpu(int64_t B, int64_t T, int64_t C, int64_t H, torch::Ten
 }
 
 
-
-NEURON_LIBRARY(my_ops, m) {
-   m.def("forward_cpu", &forward_cpu, "forward_cpu");
-}
--- src/models/simple/module/justawsshape.cpp
+++ src/models/simple/module/justawsshape.cpp
@@ -9,7 +9,7 @@ typedef float fp32;
 
 
 
-torch::Tensor forward_cpu(int64_t B, int64_t T, int64_t C, int64_t H, torch::Tensor &s, torch::Tensor &r, torch::Tensor &k, torch::Tensor &v, torch::Tensor &w, torch::Tensor &u) {
+torch::Tensor forward_cpu_shape(int64_t B, int64_t T, int64_t C, int64_t H, torch::Tensor &s, torch::Tensor &r, torch::Tensor &k, torch::Tensor &v, torch::Tensor &w, torch::Tensor &u) {
     
    torch::Tensor t_out = torch::zeros({B, H, T + (C/H), C/H});
 
@@ -19,5 +19,5 @@ torch::Tensor forward_cpu(int64_t B, int64_t T, int64_t C, int64_t H, torch::Ten
 
 
 NEURON_LIBRARY(my_ops, m) {
-   m.def("forward_cpu", &forward_cpu, "forward_cpu");
+   m.def("forward_cpu", &forward_cpu_shape, "forward_cpu_compute");
 }
